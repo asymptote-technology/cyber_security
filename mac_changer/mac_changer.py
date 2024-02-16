e9569930dc2f6ca6548383ffee62f4b3b3ddfeb3
asymptote-technology <technologyasymptote@gmail.com>
2024-02-16 08:18:36 -0500

    Added mac_changer projet

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -15,3 +15,37 @@ def get_arguments():
     if not options.new_mac:
         parser.error("[-] Please specify a new MAC address. Use --help for more info.")
     return options
+
+
+def change_mac(_interface, _new_mac):
+    print("[+] Changing MAC Address for " + _interface + " to " + _new_mac)
+    subprocess.call(["ifconfig", _interface, "down"])
+    subprocess.call(["ifconfig", _interface, "hw", "ether", _new_mac])
+    subprocess.call(["ifconfig", _interface, "up"])
+
+
+def get_current_mac(_interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", _interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("CURRENT MAC = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] The MAC address was successfully changed")
+else:
+    print("[-] The MAC address was not changed")
+
+
+
+
